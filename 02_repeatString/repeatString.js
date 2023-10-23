17e656e98f8771f8628411c9257dc82039640ccf
worsleyj <josephworsley23@gmail.com>
2023-10-23 11:36:18 -0400

    exercise 2 completed, all tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num < 0) { return 'ERROR'}
+    repeated = "";
+    for(i = 0; i < num; i++) {
+        repeated += string;
+    }
+    return repeated;
 };
 
 // Do not edit below this line
