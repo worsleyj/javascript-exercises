3d3cbf7e50b9db085bdc9b6ddad534654b644be3
worsleyj <josephworsley23@gmail.com>
2023-10-23 11:46:19 -0400

    completed exercise 3, all tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    reversed = "";
+    for(i = string.length; i >= 0; i--) {
+        reversed += string.charAt(i);
+    }
+    return reversed;
 };
 
 // Do not edit below this line
