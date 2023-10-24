86b9b69f851d23678f87c2bddb6c824116549ae5
worsleyj <josephworsley23@gmail.com>
2023-10-24 08:10:14 -0400

    exercise 7 completed, all tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let celsius = ((temperature - 32)*5)/9; // fahrenheit - 32 * (5/9) = celsius
+  return parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let fahrenheit = ((temperature * 9) / 5) + 32; // celsius * (9/5) + 32 = fahrenheit
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
